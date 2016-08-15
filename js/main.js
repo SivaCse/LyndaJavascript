7be785ee11dc57638787b540ccf43e816206735f
gareththomasnz <gareththomasnz@yahoo.com>
2016-08-15 17:11:59 +1200

    third commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+//
+//window.onload = writeMessage;
+//
+//function writeMessage(){
+//        document.getElementById("helloMessage").innerHTML = "Javascript is Boss!";
+//}
 
+//alert("Howdy");
+//
+//if(navigator.cookieEnabled){
+//        alert("Cookies yes");
+//}else{
+//    alert("cookies no");    
+//}
+//var ans = prompt("Are you sure you want to do that?","");
+//if(ans){
+//        alert("You said " + ans );
+//}else{
+//        alert("You did not answer");
+//}
+
+//if(confirm("Are you an asshole?")){
+//        alert("You are an asshole");
+//}else{
+//        alert("You are not an asshole");
+//}
+
+
+window.onload = initAll;
+
+function initAll(){
+        document.getElementById("redirect").onclick = clickHandler();
+        }
+        
+function clickHandler(){
+        alert("Ow - that hurt");
+        return false;
+}
